93fa1fd726497b0e758c5da05d98cc0d45da7133
fsdafdass <sfadfsa@fdsds.vom>
2014-03-30 21:00:20 +0300

    added post method request for register

--- src/com/zitec/workshopz/user/storage/adapters/UserWSAdapter.java
+++ src/com/zitec/workshopz/user/storage/adapters/UserWSAdapter.java
@@ -1,6 +1,7 @@
 package com.zitec.workshopz.user.storage.adapters;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -9,7 +10,10 @@ import android.content.Context;
 import android.util.Log;
 
 import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.zitec.workshopz.R;
 import com.zitec.workshopz.base.storage.adapters.BaseWSStorageAdapter;
 import com.zitec.workshopz.user.entities.User;
@@ -78,28 +82,53 @@ public class UserWSAdapter extends BaseWSStorageAdapter{
 		final String r_position = 		params.get("position");
 		final String r_password = 		params.get("password");
 		final String r_phone_number = 	params.get("phone_number");
+		
+		StringRequest postRequest = new StringRequest(Request.Method.POST, url, 
+			    new Response.Listener<String>() 
+			    {
+			        @Override
+			        public void onResponse(String response) {
+			        	JSONObject responseObj;
+			        	HashMap<String, String> data = null;
+						try {
+							responseObj = new JSONObject(response);
+				    		data = new HashMap<String, String>();
+			    			data.put("id", responseObj.getString("id"));
+						} catch (JSONException e) {
+							UserWSAdapter.this.mapper.onError(UserWSAdapter.this.context.getResources().getString(R.string.user_not_registered));
+						}
 
-		JsonObjectRequest req = new JsonObjectRequest(Request.Method.POST, url, null, this, this){
-			@Override
-		    public HashMap<String, String> getParams() {
-				HashMap<String, String> params = new HashMap<String, String>();
-				params.put("name", r_name);
-				params.put("email", r_email);
-				params.put("position", r_position);
-				params.put("password", r_password);
-				params.put("phone_number", r_phone_number);
-				return params;
-			}
-			
-			@Override
-			public HashMap<String, String> getHeaders(){
-				HashMap<String, String> params = new HashMap<String, String>();
-				//params.put("Content-Type", "application/json");
-				params.put("Accept", "application/json");
-				return params;
-			}
-		};
-		this.queue.add(req);
+			            Log.d("Response", response);
+			            UserWSAdapter.this.mapper.onSuccess(data);
+			        }
+			    }, 
+			    new Response.ErrorListener() 
+			    {
+			         @Override
+			         public void onErrorResponse(VolleyError error) {
+			             // error
+			             Log.d("Error.Response", error.getMessage());
+			       }
+
+			    }
+			) {     
+			    @Override
+			    protected Map<String, String> getParams() 
+			    {  
+			            Map<String, String>  params = new HashMap<String, String>();  
+			            params.put("name", r_name);  
+			            params.put("email", r_email);
+			            params.put("position", r_position);
+			            params.put("password", r_password);
+			            params.put("phone_number", r_phone_number);
+			             
+			            return params;  
+			    }
+			};
+			queue.add(postRequest);
 			
 	}
+	
+	
+	
 }
